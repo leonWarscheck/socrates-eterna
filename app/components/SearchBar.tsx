e0d06e27e31da304c5a6fb1be80f6d90ebc8eb45
Leon <leon.andersen@protonmail.com>
2024-10-10 17:16:11 +0200

    feat(ui): Ready for next feedback. Big parts still unresponsive. Comic page calendar ad into fixed container. SearchBar right outside, mockup sharebutton.

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -17,11 +17,41 @@ export default function SearchBar({ query, mode, setMode, isSearching }) {
 
           <ModeSelectorBar mode={mode} setMode={setMode} />
         </div>
-        <div className="size-12 bg-transparent rounded-xl border-2 border-dilred ml-auto my-auto mr-4">
-        </div>
-<div className="absolute top-44 right-0 size-96 bg-purple- 950 border-2 border-dilred rounded-xl">pantheon ad
+        <div className="size-10 bg-transparent rounded-lg border-2 border-dilred ml-auto mt-8 mr- 4">
+          <img className="size-4 mt-2.5 mx-auto" src="/main/share.svg" alt="" />
 
-</div>
+
+        </div>
+        <div className="absolute flex flex-col top-44 right-0 size-96 bg-purple- 950 border-2 border-dilred rounded-xl">
+        <div className="mx-auto mt-8 "> 
+        <img
+              className=" h-32  object-cover"
+              src="/books/calendar03.png"
+              alt=""
+            />
+
+        </div>
+        <div className="flex flex-col mr-4 pr-6 ml-6 text-center">
+                <h2 className="font-bold py-3 text-xl">
+                  2025 Page-A-Day Calendar
+                </h2>
+
+              <p className="overflow-hidden text-base  text- justify hyphens-auto mb-3">
+                <span className="font-bold">Available Now! </span>
+
+                Back by popular demand, now featuring both <span className="font-semibol italic">Socrates Eterna and </span> <span className="font-semibol italic">Socrates Pantheon!</span>  2 comics each day, printed in Athens!
+                <br />
+              </p>
+              <Link
+                to="https://www.amazon.com"
+                className="font-normal text-sm  hover:underline bg-purple-950  h-8 px-4 pt-1  mx-auto 4 border-2 border- rounded-lg "
+                target="_blank"
+                rel="noopener norefferrer"
+              >
+                click to shop
+              </Link>
+            </div>
+        </div>
       </div>
     </div>
   );
