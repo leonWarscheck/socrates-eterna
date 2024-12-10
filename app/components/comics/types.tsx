90870a6c60b811f19562e8ae64683693dcf04247
Leon <leon.andersen@protonmail.com>
2024-12-10 12:39:32 +0100

    refactor: type structure prep 1

--- app/components/comics/types.tsx
+++ app/components/comics/types.tsx
@@ -1,4 +1,5 @@
-import type { ReactNode } from 'react';
+import type { ReactNode, ComponentProps } from 'react';
+
 
 export interface Props{
     children?: ReactNode;
@@ -8,23 +9,35 @@ export interface Props{
     results?: LoaderResults;
 }
 
+// type MyComponent1Props = { title: string;
+// }
+// type MyComponent2Props = { age: number;}
+// type MyParentComponentProps = MyComponent1Props & MyComponent2Props & {blah: boolean;}
+// const myObject: MyParentComponentProps = {title: "blah"}
+function MyComponent (props: {foo: string; bar: number;}){return null}
+type MyComponentProps = ComponentProps<typeof MyComponent>
+
+type MyPartenComponentProps = {title: string; age: number;}
+type MyComponent1Props = Pick<MyPartenComponentProps, "title" | "age"> 
+
 export interface Character {
     name: string;
     image: string;
   }
 
 export interface Comic {
-    id?: string;
-    values?: number[];
+    id: string;
     metadata: ComicMetadata
 }
 
 export interface ComicMetadata {
     filename: string;
     published: string;
-    title: string;
+    title: number;
     content: string;
 }
+
+type myComponentProp = ComicMetadata["title"]
 
 export type LoaderResults = Comic[]
 
