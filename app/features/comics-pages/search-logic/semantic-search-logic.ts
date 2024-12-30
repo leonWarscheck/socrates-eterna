596a6f7448d6ea7f244b32979107c325baeba07d
Leon <leon.andersen@protonmail.com>
2024-12-30 10:56:42 +0100

    docs(semanticSearch): tsdoc reformat

--- app/features/comics-pages/search-logic/semantic-search-logic.ts
+++ app/features/comics-pages/search-logic/semantic-search-logic.ts
@@ -5,9 +5,10 @@ import { getQueryEmbeddings } from "./get-query-embeddings.server";
  * The core search engine used inside the search-helpers in the loaders of
  * `results.meaning` and `results.character`.
  *
- * Works for both search types, because character names are part of the comic
+ * @remarks Works for both search types, because character names are part of the comic
  * scripts at `comic.metadata.content`, from which the embeddings have been
  * generated.
+ * 
  * @param query - Query string from `request` -> `loader` -> `getNewResultsAndQuery` -> to here.
  * @returns An array of cleaned up comic data objects, that are ready to be passed down to and rendered in `render-results`.
  */
